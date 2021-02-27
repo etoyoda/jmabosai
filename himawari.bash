7bc5e5764ee8e0d8f4324e015fd92b5fd8679f59
TOYODA Eizi <toyoda.eizi@gmail.com>
2021-02-28 03:11:38 +0900

    * 取得対象時間を選択可能に * 赤外と水蒸気画像を取得 * つなげてみられるHTMLを用意

--- himawari.bash
+++ himawari.bash
@@ -1,37 +1,84 @@
 #!/bin/bash
 set -Ceuo pipefail
+#
+# ひまわり画像をダウンロードする
+#
 PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin
 LANG=C
 TZ=UTC
 
-# products
-products="B13/TBB "
+# === 設定 ===
+# 取得画種
+products="B13/TBB B08/TBB"
+# ズームレベル
+z=5
+# 取得間隔（秒）
+# interval=3600
+interval=10800
 
-# move to directory in which THIS script is located
-cd $(dirname $0)
-
-wget -Ozt-himawari.json -q \
-  https://www.jma.go.jp/bosai/himawari/data/satimg/targetTimes_fd.json
-
-set $(ruby -rjson -e 'h=JSON[File.read(ARGV.first)].last; puts h["basetime"]; puts h["validtime"]' zt-himawari.json)
-basetime=$1
-validtime=$2
-
-if test -d $basetime; then
-  echo $basetime already exists
-  exit 0
-fi
-mkdir -p himawari/$basetime
-pushd himawari/$basetime
+# データ保存場所は $JMADATADIR、未設定時はスクリプト設置場所
+cd ${JMADATADIR:-$(dirname $0)}
+mkdir -p himawari
+cd himawari
 
 root=https://www.jma.go.jp/bosai/himawari/data/satimg
-z=5
 
-for prod in $products
+# 時間リストを取得してプレーンテキストに変換する
+# ひまわりの場合 basetime=validtime なので basetime だけを出力
+wget -OtargetTimes_fd.json -q ${root}/targetTimes_fd.json
+ruby -rjson -e 'JSON[File.read(ARGV.first)].each{|h| puts h["basetime"]}' \
+  targetTimes_fd.json > targetTimes_fd.txt
+rm -f targetTimes_fd.json
+
+while read basetime
 do
-  ruby -e '(25..30).each{|x|(10..15).each{|y| puts "#{ARGV[0]}/#{x}/#{y}.jpg"}}' ${root}/${basetime}/fd/${validtime}/${prod}/${z} > ../zlist.txt
-  wget -q -x -nH --cut-dirs=7 -i ../zlist.txt
-  rm -f ../zlist.txt
-done
-popd
-rm -f zt-himawari.json
+  validtime=$basetime
+
+  itime=$(ruby -rtime -e 'puts Time.parse(ARGV.first).to_i' $basetime)
+  if expr $itime \% $interval '!=' 0 >/dev/null
+  then
+    : skipping $basetime
+    continue
+  fi
+
+  if test -d $basetime; then
+    : $basetime already exists
+    continue
+  fi
+  mkdir -p $basetime
+  pushd $basetime
+
+  for prod in $products
+  do
+    ruby -e '(25..30).each{|x|(10..15).each{|y|
+      puts "#{ARGV[0]}/#{x}/#{y}.jpg"}}' \
+      ${root}/${basetime}/fd/${validtime}/${prod}/${z} > ../zlist.txt
+    wget -q -x -nH --cut-dirs=7 -i ../zlist.txt
+    rm -f ../zlist.txt
+    # タイルのまま連結表示するHTMLを書き出す
+    ruby -e 'bt, pr = ARGV
+      fn = pr.sub(/\//, "_") + ".html"
+      $stdout = File.open(fn, "w")
+      puts <<-HTML
+        <html>
+        <head><style type="text/css">
+        table { border-collapse: collapse; }
+        tr td { padding: 0; border: 0;}
+        </style>
+        </head>
+        <body><table>
+        HTML
+      (10..15).each{|y|
+        puts "<tr>"
+        (25..30).each{|x|
+          puts "<td><img src='\''#{pr}/5/#{x}/#{y}.jpg'\''></td>"
+        }
+        puts "</tr>"
+      }
+      puts "</table></body></html>"
+    ' $basetime $prod
+  done
+  popd
+
+done < targetTimes_fd.txt
+rm -f targetTimes_fd.txt
